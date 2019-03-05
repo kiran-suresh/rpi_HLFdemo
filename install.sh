01d0afc4466dfd2fbac9e049f99d842f0fc60139
kiran-suresh <kiran.suresh@asu.edu>
2019-03-05 12:50:18 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,20 +6,20 @@ else
     # command
     curl -sSL https://get.docker.com | sh
 fi
-docker pull pesicsasa/fabric-couchdb
-docker pull pesicsasa/fabric-ca-peer
-docker pull pesicsasa/fabric-buildenv
-docker pull pesicsasa/fabric-testenv
-docker pull pesicsasa/fabric-ca-tools
-docker pull pesicsasa/fabric-ca-orderer
-docker pull pesicsasa/fabric-basejvm
-docker pull pesicsasa/fabric-baseos
-docker pull pesicsasa/fabric-tools
-docker pull pesicsasa/fabric-baseimage
-docker pull pesicsasa/fabric-ccenv
-docker pull pesicsasa/fabric-orderer
-docker pull pesicsasa/fabric-zookeeper
-docker pull pesicsasa/fabric-kafka
-docker pull pesicsasa/fabric-ca
-docker pull pesicsasa/fabric-peer
-docker pull pesicsasa/fabric-javaenv
+sudo docker pull pesicsasa/fabric-couchdb
+sudo docker pull pesicsasa/fabric-ca-peer
+sudo docker pull pesicsasa/fabric-buildenv
+sudo docker pull pesicsasa/fabric-testenv
+sudo docker pull pesicsasa/fabric-ca-tools
+sudo docker pull pesicsasa/fabric-ca-orderer
+sudo docker pull pesicsasa/fabric-basejvm
+sudo docker pull pesicsasa/fabric-baseos
+sudo docker pull pesicsasa/fabric-tools
+sudo docker pull pesicsasa/fabric-baseimage
+sudo docker pull pesicsasa/fabric-ccenv
+sudo docker pull pesicsasa/fabric-orderer
+sudo docker pull pesicsasa/fabric-zookeeper
+sudo docker pull pesicsasa/fabric-kafka
+sudo docker pull pesicsasa/fabric-ca
+sudo docker pull pesicsasa/fabric-peer
+sudo docker pull pesicsasa/fabric-javaenv
